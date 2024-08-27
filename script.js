c53c813979c50449ddc1c3abd777149d143e319c
Riquegamer <hiquehique12348@gmail.com>
2024-08-26 22:46:20 -0300

    imagens pt1

--- script.js
+++ script.js
@@ -21,4 +21,22 @@ document.addEventListener('DOMContentLoaded', function() {
          }
      });
  });
- 
+
+ document.addEventListener('DOMContentLoaded', function() {
+    const audio = new Audio('AUDIO_CHINAMUITOLOCA.mp3');
+    audio.volume = 0.5;
+    audio.loop = true;
+
+    // Função para tentar iniciar a reprodução do áudio
+    function playAudio() {
+        audio.play().catch(function(error) {
+            console.log('Erro ao tentar tocar a música:', error);
+        });
+    }
+
+    // Tenta tocar o áudio automaticamente
+    playAudio();
+
+    // Adiciona um manipulador de eventos para garantir que o áudio seja tocado após interação do usuário, se necessário
+    document.addEventListener('click', playAudio);
+});
